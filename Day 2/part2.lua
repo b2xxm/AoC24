7ca74ffab0fecfd344512390279ecca76f52eae8
b2xxm <149617114+b2xxm@users.noreply.github.com>
2024-12-05 22:29:22 +0000

    update day 2 part 2

--- Day 2/part2.lua
+++ Day 2/part2.lua
@@ -5,10 +5,10 @@ for v in t:gmatch("[^\n]+") do
     ::b:: s,p,i,d,t=1,0,0,0,1;if r==1 then j,k=1,#l else j,k=k,j end
     for x=j,k,r do
         c=l[x];if p==0 then goto e end
-        if c>p then if d==1 then if t==1 then t=0 goto x end s=0 break end i=1 end
-        if c<p then if i==1 then if t==1 then t=0 goto x end s=0 break end d=1 end
-        a=math.abs(p-c);if a<1 or a>3 then if t==1 then t=0 goto x end s=0 break end
-        ::e:: p=c ::x::
+        if c>p then if d==1 then if t==1 then t=0 goto f end s=0 break end i=1 end
+        if c<p then if i==1 then if t==1 then t=0 goto f end s=0 break end d=1 end
+        a=math.abs(p-c);if a<1 or a>3 then if t==1 then t=0 goto f end s=0 break end
+        ::e:: p=c ::f::
     end
     if s==0 and r==1 then r=-1 goto b elseif s==1 then n=n+1 end
 end

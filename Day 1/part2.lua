b51be800f7b1d3f028f2a545655e801a8e3385f6
b2xxm <149617114+b2xxm@users.noreply.github.com>
2024-12-05 01:18:36 +0000

    update day 1 part 2

--- Day 1/part2.lua
+++ Day 1/part2.lua
@@ -1,5 +1,4 @@
 s=io.read("*all")
-t,t1,t2,n={},{},{},0
-for a,b in s:gmatch("(%d+)%s+(%d+)")do table.insert(t1,a);table.insert(t2,b) end
-for _,v in pairs(t1) do if t[v] then t[v]=t[v]+1 else t[v]=1 end end
+t,t2,n={},{},0
+for a,b in s:gmatch("(%d+)%s+(%d+)")do if t[a] then t[a]=t[a]+1 else t[a]=1 end table.insert(t2,b) end
 for _,v in pairs(t2) do if t[v] then n=n+v*t[v] end end;print(n)

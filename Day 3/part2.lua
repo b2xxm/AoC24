2e8ff752688c8217dc4e1e7b37a657151042d9fe
b2xxm <149617114+b2xxm@users.noreply.github.com>
2024-12-05 19:20:18 +0000

    update day 3 part 2

--- Day 3/part2.lua
+++ Day 3/part2.lua
@@ -4,8 +4,8 @@ for m in s:gmatch("do%(%)()")do table.insert(te,m) end
 for m in s:gmatch("don't%(%)()")do table.insert(td,m) end
 ei,ev,di,dv,n=1,0,1,0,0
 for i,a,b in s:gmatch("()mul%((%d+),(%d+)%)") do
-	while true do v=td[di];if v and i>=v then dv=v;di=di+1 goto c1 end di=di-1 break ::c1:: end
-	while true do v=te[ei];if v and i>=v then ev=v;ei=ei+1 goto c2 end ei=ei-1 break ::c2:: end
-	if not (ev>=dv) then goto c3 end n=n+a*b ::c3::
+	while true do v=td[di];if v and i>=v then dv=v;di=di+1 else di=di-1 break end end
+	while true do v=te[ei];if v and i>=v then ev=v;ei=ei+1 else ei=ei-1 break end end
+	if ev>=dv then n=n+a*b end
 end
 print(n)

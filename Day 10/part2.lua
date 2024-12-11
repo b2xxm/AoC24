2655ba920443b261381a912e35df7cb1f33ecdbe
b2xxm <149617114+b2xxm@users.noreply.github.com>
2024-12-11 02:37:45 +0000

    day 10 part 2
    why is part 2 easier than part 1, i also accidentally did part 2 before part 1 somehow

---
+++ Day 10/part2.lua
@@ -0,0 +1,25 @@
+s=io.read("*all")
+td={{1,0},{-1,0},{0,1},{0,-1}}
+t,n={},0
+for l in s:gmatch("[^\n]+") do
+    tr={};table.insert(t,tr)
+    for h in l:gmatch(".") do table.insert(tr,tonumber(h)) end
+end
+function path(r,c,h)
+    if h==9 then
+        n=n+1 return
+    end
+    for _,d in pairs(td) do
+        ar,ac=r+d[1],c+d[2]
+        tr=t[ar] if not tr then goto a1 end
+        ah=tr[ac] if not ah then goto a1 end
+        if ah~=h+1 then goto a1 end
+        path(ar,ac,ah) ::a1::
+    end
+end
+for r,tr in pairs(t) do
+    for c,h in pairs(tr) do
+        if h==0 then path(r,c,h) end
+    end
+end
+print(n)

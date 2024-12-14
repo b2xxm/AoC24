da0f7fd930b48da73429c1374da37b906941806e
b2xxm <149617114+b2xxm@users.noreply.github.com>
2024-12-14 07:54:25 +0000

    update day 13 part 1

--- Day 13/part1.lua
+++ Day 13/part1.lua
@@ -1,13 +1,9 @@
 s=io.read("*all")
 n=0
 for ax,ay,bx,by,px,py in s:gmatch("(%d+).-(%d+).-(%d+).-(%d+).-(%d+).-(%d+)") do
-    l,b,px,py,cpx,cpy=math.huge,0,tonumber(px),tonumber(py),0,0
-    while true do
-        dpx,dpy=px-cpx,py-cpy
-        if dpx%ax==0 and dpy%ay==0 and dpx/ax==dpy/ay and dpx/ax*b<l then l=dpx/ax*3+b end
-        b,cpx,cpy=b+1,cpx+bx,cpy+by
-        if cpx>px or cpy>py then break end
-    end
-    if l~=math.huge then n=n+l end
+    d=(ax*by-ay*bx) if d==0 then goto continue end
+    px,py=px+0,py+0
+    a,b=math.floor((by*px-bx*py)/d+0.5),math.floor((ax*py-ay*px)/d+0.5)
+    if a*ax+b*bx==px and a*ay+b*by==py then n=n+a*3+b end ::continue::
 end
 print(n)
